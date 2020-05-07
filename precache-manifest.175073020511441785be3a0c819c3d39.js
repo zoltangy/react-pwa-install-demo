56eba1e3edb1c799bfb3a15f6e12584b4b6333df
zoltangy <zoltangy@whatever.com>
2020-05-07 15:18:29 +0200

    Updates

---
+++ precache-manifest.175073020511441785be3a0c819c3d39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1903a1f2a89dd21b58f5c6d766d984",
+    "url": "/react-pwa-install-demo/index.html"
+  },
+  {
+    "revision": "b8a1b78d8f114b7a5a62",
+    "url": "/react-pwa-install-demo/static/js/2.bc71655f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-pwa-install-demo/static/js/2.bc71655f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1494a9b797e8c3fb2b",
+    "url": "/react-pwa-install-demo/static/js/main.cd38e233.chunk.js"
+  },
+  {
+    "revision": "6851b0cf0a58996db15d",
+    "url": "/react-pwa-install-demo/static/js/runtime-main.602f8855.js"
+  }
+]);
