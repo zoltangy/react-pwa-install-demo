98b97293b4b3870c3f412a329d1f8bd7d6383d28
zoltangy <zoltangy@whatever.com>
2020-05-07 15:17:13 +0200

    demo implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useReactPWAInstall } from "react-pwa-install";
+import myLogo from "./logo70.png";
 
 function App() {
+  const { pwaInstall, supported, isInstalled } = useReactPWAInstall();
+
+  const handleClick = () => {
+    pwaInstall({
+      title: "Install Web App",
+      logo: myLogo,
+      features: (
+        <ul>
+          <li>Cool feature 1</li>
+          <li>Cool feature 2</li>
+          <li>Even cooler feature</li>
+          <li>Works offline</li>
+        </ul>
+      ),
+      description: "This is how the install dialog looks like. Here you can describe your app briefly.",
+    })
+      .then(() => alert("App installed successfully or instructions for install shown"))
+      .catch(() => alert("User opted out from installing"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        If your browser is supported and you haven't already installed the app, you should see the install
+        button below:
+      </p>
+      {supported() && !isInstalled() && (
+        <button type="button" onClick={handleClick}>
+          Install app
+        </button>
+      )}
     </div>
   );
 }
